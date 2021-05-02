05dfbdc2b56331bf3733f9d5417f15db0d0f72cc
Mickle <git@indiro.ru>
2021-05-02 22:10:14 +0300

    merge

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, NavLink } from 'react-router-dom';
-import { LayoutComponent } from 'layout';
+import { LayoutComponent } from 'src';
 import leftMenuItems from './data/leftMenuItems';
 
 import 'layout/dist/index.css'
--- src/build.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Layout } from './template/Layout';
-import './style.css';
-
-export {
-    Layout
-};
